14cd8a2f9de39bb9cf7383a81503281be61c98a8
bhagyalaxmi25 <palaibhagyalaxmi81@gmail.com>
2024-10-07 12:00:47 +0530

    clg_project2

--- FrontEnd/JavaScript/Class2/index.js
+++ FrontEnd/JavaScript/Class2/index.js
@@ -1,56 +1,88 @@
-console.log(city);
+// console.log(city);
 
-var city = "Kdp";
-console.log(city);
+// var city = "Kdp";
+// console.log(city);
 
 
 
-let user;
-user = null;
-console.log(typeof null);       //Output- Object
-console.log(user);
+// let user;
+// user = null;
+// console.log(typeof null);       //Output- Object
+// console.log(user);
 
 
 // Undefined
-let data;
-console.log(typeof data);
-console.log(data);
+// let data;
+// console.log(typeof data);
+// console.log(data);
 
 
 // Number
-let num1 = 50;
-let decimal_no = 1.0101;
-let pi = 3.141;
-let age = 21;
-let roll_no = 23;
+// let num1 = 50;
+// let decimal_no = 1.0101;
+// let pi = 3.141;
+// let age = 21;
+// let roll_no = 23;
 
-console.log(typeof decimal_no);
-console.log(decimal_no);
+// console.log(typeof decimal_no);
+// console.log(decimal_no);
 
 
 
 // Add
-let n1 = 29;
-let n2 = 25;
-let addition = n1 + n2;
-console.log(addition);
+// let n1 = 29;
+// let n2 = 25;
+// let addition = n1 + n2;
+// console.log(addition);
 
 // Sub
-let n3 = 80;
-let n4 = 25;
-let subtract = n3 - n4;
-console.log(subtract);
+// let n3 = 80;
+// let n4 = 25;
+// let subtract = n3 - n4;
+// console.log(subtract);
 
 // Mul
-let n5 = 50;
-let n6 = 75;
-let multiplication = n5 * n6;
-console.log(multiplication);
+// let n5 = 50;
+// let n6 = 75;
+// let multiplication = n5 * n6;
+// console.log(multiplication);
 
 // Div
-let n7 = 70;
-let n8 = 5;
-let division = n7 / n8;
-console.log(division);
+// let n7 = 70;
+// let n8 = 5;
+// let division = n7 / n8;
+// console.log(division);
+
+
+
+
+
+// Nan
+// let num2 = 0;
+// let num3 = 0;
+// console.log(num2 / num3);
+
+// let num4 = "Jinu";
+// let num5 = 29;
+// console.log(num4 / num5);
+
+
+
+// String = Collection of Charecters
+let Jname = 'Jinu';
+let Bname = "Bikash";
+console.log(Jname,Bname);
+
+
+
+
+let myName = "Bhagyalaxmi";
+let age = 21;
+let city = "Kendrapara";
+let roll_no = 23;
+let College = "USBM";
+let Batch = "MCA(2023 - 2025)";
+
+console.log(`My name is ${myName} , age is ${age} , city in ${city} , roll number is ${roll_no} in the college ${College} and batch ${Batch}`);
 
 
