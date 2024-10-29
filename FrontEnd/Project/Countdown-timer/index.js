cec2f6cf7ecb96f0a07fd8b531b188f7c7190338
bhagyalaxmi25 <palaibhagyalaxmi81@gmail.com>
2024-10-29 23:45:00 +0530

    Image-slider

--- FrontEnd/Project/Countdown-timer/index.js
+++ FrontEnd/Project/Countdown-timer/index.js
@@ -1,13 +1,17 @@
 let timer;
 let timeInput;
-let timeOutput = document.getElementById('timeOutput');
+let timeOutput = document.getElementById("timeOutput");
 
 function setTime() {
-  let timeInput = document.getElementById('timeInput');
+  let timeInput = document.getElementById("timeInput");
   let time = parseInt(timeInput.value);
 
   timer = setInterval(() => {
     if (time > 0) {
+      const minutes = Math.floor(time / 60);
+      const seconds = time % 60;
+      timer.textContent = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+
       timeOutput.innerHTML = time;
     } else {
       timeOutput.innerHTML = "Time's Up";
